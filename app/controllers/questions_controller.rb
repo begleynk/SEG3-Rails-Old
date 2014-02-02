ca0d087acdc459e4d86bacc018856a5546bfb7b3
Niklas Begley <niklas.begley@gmail.com>
2014-02-02 14:01:33 +0000

    Configured routing for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,42 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :update, :destroy]
 
   def show
-    @question = Question.find(params[:id])
-  end
-
-  def edit
+    render json: @question.to_json(include: :question_options), status: :ok
   end
 
   def create
+    @question = Question.new(question_params)
+    if @question.save
+      render json: @question.to_json(include: :question_options), status: :ok
+    else
+      render json: @question.errors, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @question.update
+      render json: @question.to_json, status: :ok
+    else
+      render json: @question.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    if @question.destroy
+      render json: @question.to_json, status: :ok
+    else
+      render json: @question.errors, status: :unprocessable_entity
+    end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_question 
+      @question = Question.find(params[:id])
+    end
+    
+    def question_params
+      params.require(:question).permit(:title, :subtext, :questionnaire_id, :question_type)
+    end
 end
